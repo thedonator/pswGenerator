6ecb0951ac4e90cfcee83a2804b0740dbe939f4f
Stone <valepie@icloud.com>
2020-08-04 20:57:08 +0200

    Major bug fixes

--- src/main.c
+++ src/main.c
@@ -1,58 +1,82 @@
 #include <stdio.h>
+#include <stdbool.h> 
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
 
 
-
+void greetings(void);
 int config(void);
-void openCreateFile(char *fileName,  int pswLenght);
+void printAdvice(int nCharacters);
+bool choice(void);
+void openCreateFile(char *fileName,  int pswLenght);                              
 //void generatePsw();
 
-
-int config(void){
- 
-    int pswLenght = 0;
-    
+void greetings(void)
+{
     printf("Welcome to password generator\n");
     printf("Insert number of characters required "
     "for the psw: ");
-    scanf("%d",&pswLenght);
-    
-    if (pswLenght < 0 || pswLenght == 0)
+}
+void printAdvice(int nCharacters)
+{
+    if (nCharacters < 0 || nCharacters == 0)
     {
-        while (pswLenght <= 0)
+        while (nCharacters <= 0)
         {
             fprintf(stderr,"The password lenght should be "
             "at least 1 character long, for maximum security "
             "is preferable at least 8 characters\n");
             fprintf(stderr, "Please reinsert the lenght of the password: ");
-            scanf("%d",&pswLenght);
+            scanf("%d",&nCharacters);
         }
     }
-    return pswLenght;
 
+}
+
+bool choice(void)
+{
+    int inpValue;
+    printf("If you are ok with you choices press 1 else press 0: ");
+    scanf("%d",&inpValue);
+    return inpValue == 1 ? true : false;
+}
+
+int config(void){
+ 
+    int pswLenght = 0;
+    bool confirmation = true;
+
+    greetings();
+    scanf("%d",&pswLenght);
+    printAdvice(pswLenght);
+    confirmation = choice();
+    
+    while(confirmation == false)
+    {
+        pswLenght = 0;
+        greetings();
+        scanf("%d",&pswLenght);
+        printAdvice(pswLenght);
+        confirmation = choice();
+        //config();
+    }
+    return pswLenght;
 }
 
 void openCreateFile(char *fileName,  int pswLenght){
     FILE *file;
     file = fopen(fileName,"a");
-    
-    if(file == NULL)
-    {
-        fprintf(stderr, "Generated %s",fileName);
-        file = fopen(fileName,"w");
-        file = fopen(fileName,"a+");
-    }
-
     fprintf(file, "Log:\n\tpassword lenght: %d\n", pswLenght);
     fclose(file);
 }
 
 
 int main(int argc, char *argv[]){
-    int pswLenght = 0;
+    int pswLenght;
     pswLenght = config();
+    assert(pswLenght == 88);
     printf("value = %d \n",pswLenght);
     openCreateFile(argv[1],pswLenght);
+    return 0;
 }
